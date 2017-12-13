ad1c902e264a42d950b925a79be2b383eaaa55d0
NRothera <31472463+NRothera@users.noreply.github.com>
2017-12-13 11:42:55 +0000

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # sparta-json-parsing
+
+
+Putting blocks of data and turning it into a readable format
+
+Making code reusable 
---
+++ json_walkthrough.rb
@@ -0,0 +1,18 @@
+require 'json'
+
+json = File.read('json_example.json')
+
+json_parse = JSON.parse(File.read('json_example.json'))
+
+
+
+p json_parse['colors'].first
+
+
+
+
+
+
+
+
+#
